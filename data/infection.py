f4131c3c33321398e36bb525d8c13e0812cc3a10
soeon.park <soeon.park@linecorp.com>
2021-04-03 12:28:26 +0900

    fix dir

--- data/infection.py
+++ data/infection.py
@@ -1,7 +1,7 @@
 import requests
 
 URL = 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv'
-DIR = '/Users/JG20021/Desktop/soeon/HSU/2021_spring/CS_480/MLproject/data/'
+DIR = './'
 FILE = 'infection_by_county.csv'
 
 req = requests.get(URL)
--- data/vaccination.py
+++ data/vaccination.py
@@ -1,7 +1,7 @@
 import requests
 
 URL = 'https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/us_state_vaccinations.csv'
-DIR = '/Users/JG20021/Desktop/soeon/HSU/2021_spring/CS_480/MLproject/data/'
+DIR = './'
 FILE = 'vaccination_by_state.csv'
 
 req = requests.get(URL)
